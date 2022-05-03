55bac11c29e516ecb7e06e7f007243e5cebf8b79
imbaya <kagamigawa1mhaya0@gmail.com>
2022-05-03 22:44:53 +0800

    平滑移動
    平滑移動

--- godot/Player/Player.cs
+++ godot/Player/Player.cs
@@ -7,17 +7,29 @@ public class Player : KinematicBody2D
   // private int a = 2;
   // private string b = "text";
   // Called when the node enters the scene tree for the first time.
-  public override void _Ready()
-  {
-    GD.Print("Player Ready");
-  }
+
+  public int MaxSpeed = 160;
+  public int Acceleration = 20;
+  public int Friction = 1;
+ 
+  public Vector2 Velocity = Vector2.Zero;
+
   public override void _PhysicsProcess(float delta)
   {
     base._PhysicsProcess(delta);
-    Vector2 velocity= Vector2.Zero;
-    velocity.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
-    velocity.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
-    MoveAndCollide(velocity);
-  }
+    Vector2 input_velocity= Vector2.Zero;
+    input_velocity.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+    input_velocity.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
+    input_velocity = input_velocity.Normalized();
 
+    if (input_velocity == Vector2.Zero) {
+      Velocity = Velocity.MoveToward(Vector2.Zero,Friction);
+    } else {
+      Velocity += input_velocity*Acceleration*delta;
+      Velocity = Velocity.Clamped(MaxSpeed *delta);
+    }
+
+    GD.Print(Velocity);
+    MoveAndCollide(Velocity);
+  }
 }
